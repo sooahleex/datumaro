3cbadf2f11753997cc31fa60619b574b3c2bc6f3
sooahleex <sooah.lee@intel.com>
2022-11-10 22:51:32 +0900

    use string for hash_key and test for coco, cifar10

--- datumaro/components/model_inference.py
+++ datumaro/components/model_inference.py
@@ -399,48 +399,6 @@ class CLIP(nn.Module):
         # shape = [global_batch_size, global_batch_size]
         return logits_per_image, logits_per_text
 
-_MODELS = {
-    "RN50": "https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt",
-    "RN101": "https://openaipublic.azureedge.net/clip/models/8fa8567bab74a42d41c5915025a8e4538c3bdbe8804a470a72f30b0d94fab599/RN101.pt",
-    "RN50x4": "https://openaipublic.azureedge.net/clip/models/7e526bd135e493cef0776de27d5f42653e6b4c8bf9e0f653bb11773263205fdd/RN50x4.pt",
-    "RN50x16": "https://openaipublic.azureedge.net/clip/models/52378b407f34354e150460fe41077663dd5b39c54cd0bfd2b27167a4a06ec9aa/RN50x16.pt",
-    "RN50x64": "https://openaipublic.azureedge.net/clip/models/be1cfb55d75a9666199fb2206c106743da0f6468c9d327f3e0d0a543a9919d9c/RN50x64.pt",
-    "ViT-B/32": "https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
-    "ViT-B/16": "https://openaipublic.azureedge.net/clip/models/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt",
-    "ViT-L/14": "https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt",
-    "ViT-L/14@336px": "https://openaipublic.azureedge.net/clip/models/3035c92b350959924f9f00213499208652fc7ea050643e8b385c2dac08641f02/ViT-L-14-336px.pt",
-}
-
-def _download(url: str, root: str):
-    os.makedirs(root, exist_ok=True)
-    filename = os.path.basename(url)
-
-    expected_sha256 = url.split("/")[-2]
-    download_target = os.path.join(root, filename)
-
-    if os.path.exists(download_target) and not os.path.isfile(download_target):
-        raise RuntimeError(f"{download_target} exists and is not a regular file")
-
-    if os.path.isfile(download_target):
-        if hashlib.sha256(open(download_target, "rb").read()).hexdigest() == expected_sha256:
-            return download_target
-        else:
-            warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
-
-    with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
-        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
-            while True:
-                buffer = source.read(8192)
-                if not buffer:
-                    break
-
-                output.write(buffer)
-                loop.update(len(buffer))
-
-    if hashlib.sha256(open(download_target, "rb").read()).hexdigest() != expected_sha256:
-        raise RuntimeError("Model has been downloaded but the SHA256 checksum does not not match")
-
-    return download_target
 
 def load_model(download_root: str = None, model_name: str = "ViT-B/32", jit: bool=False):
     # model, _ = clip.load("ViT-B/32", device=device, jit=True)
@@ -596,7 +554,7 @@ def inference(image):
     img_features = img_features.cpu()
     hash_key = img_features.detach().numpy() >= 0
     hash_key = hash_key*1
-    # hash_key = np.packbits(img_features, axis=-1)
-    # hash_key = list(map(lambda row: ''.join(['{:02x}'.format(r) for r in row]), hash_key))
+    hash_string = np.packbits(hash_key, axis=-1)
+    hash_string = list(map(lambda row: ''.join(['{:02x}'.format(r) for r in row]), hash_string))
 
-    return hash_key
+    return hash_string
--- datumaro/components/searcher.py
+++ datumaro/components/searcher.py
@@ -26,6 +26,25 @@ def calculate_hamming(B1, B2):
 def hamming_distance(chaine1, chaine2):
     return sum(c1 != c2 for c1, c2 in zip(chaine1, chaine2))
 
+# class Solution(object):
+#    def hammingDistance(self, x, y):
+#       """
+#       :type x: int
+#       :type y: int
+#       :rtype: int
+#       """
+#       ans = 0
+#       for i in range(31,-1,-1):
+#          b1= x>>i&1
+#          b2 = y>>i&1
+#          ans+= not(b1==b2)
+#          #if not(b1==b2):
+#             # print(b1,b2,i)
+#       return ans
+# ob1 = Solution()
+# print(ob1.hammingDistance(7, 15))
+
+
 
 class Searcher:
     def __init__(
@@ -55,16 +74,33 @@ class Searcher:
 
         # query_key = np.array([int(item.hash_key[0], 36)])
         query_key = item.hash_key[0]
+        query_key_list = [query_key[i:i+2] for i in range(0, len(query_key), 2)]
+        query_key = np.array([int(s, 16) for s in query_key_list], dtype='uint8')
+        query_key = np.unpackbits(query_key, axis=-1)
+        # query_key = np.reshape(query_key, (1, -1))
+        # query_key_string = np.packbits(query_key, axis=-1)
+        # query_key_string = list(map(lambda row: ''.join(['{:02x}'.format(r) for r in row]), [query_key_string]))
 
         retrieval_keys = []
-        id_list = []
+        # retrieval_keys_string =[]
+        # id_list = []
+        item_list = []
         for datasetitem in self._dataset:
             # hash_key = int(datasetitem.hash_key[0], 36)
             hash_key = datasetitem.hash_key[0]
+            hash_key_list = [hash_key[i:i+2] for i in range(0, len(hash_key), 2)]
+            hash_key = np.array([int(s, 16) for s in hash_key_list], dtype='uint8')
+            hash_key = np.unpackbits(hash_key, axis=-1)
+            # hash_key = np.reshape(hash_key, (1, -1))
+            # hash_string = np.packbits(hash_key, axis=-1)
+            # hash_string = list(map(lambda row: ''.join(['{:02x}'.format(r) for r in row]), [hash_string]))
             retrieval_keys.append(hash_key)
-            id_list.append(datasetitem.id)
+            # retrieval_keys_string.append(hash_string)
+            # id_list.append(datasetitem.id)
+            item_list.append(datasetitem)
         
         retrieval_keys = np.stack(retrieval_keys, axis=0)
+        # retrieval_keys_string = np.stack(retrieval_keys_string, axis=0)
         # retrieval_keys = torch.stack(retrieval_keys, dim=1).to(device = "cpu")
 
         # logits = 100. * query_key @ retrieval_keys
@@ -75,7 +111,11 @@ class Searcher:
         # retrieval_keys = retrieval_keys[ind] # reorder gnd
 
         # tgnd = retrieval_keys[0:topk]
-        id_list = np.array(id_list)[ind]
-        result = id_list[:topk].tolist()
+        
+        # id_list = np.array(id_list)[ind]
+        # result = id_list[:topk].tolist()
+
+        item_list =np.array(item_list)[ind]
+        result = item_list[:topk].tolist()
 
         return result
