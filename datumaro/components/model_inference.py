e88bd14ca3a21b92febb26baa2766d74d02c9fa3
sooahleex <sooah.lee@intel.com>
2022-11-15 00:57:07 +0900

    Use text for query

--- datumaro/components/model_inference.py
+++ datumaro/components/model_inference.py
@@ -3,14 +3,16 @@
 # SPDX-License-Identifier: MIT
 
 import os
-
-import hashlib
-import urllib
 import warnings
+import gzip
+import html
 
 from collections import OrderedDict
-from typing import Union, Tuple
-from tqdm import tqdm
+from typing import Union, Tuple, List
+from functools import lru_cache
+from pkg_resources import packaging
+import regex as re
+import ftfy
 
 import numpy as np
 from PIL import Image
@@ -20,6 +22,9 @@ from torch import nn
 from torchvision import transforms
 import torch.nn.functional as F
 from torch.autograd import Function
+
+if packaging.version.parse(torch.__version__) < packaging.version.parse("1.7.1"):
+    warnings.warn("PyTorch version 1.7.1 or higher is recommended")
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -121,6 +126,7 @@ class AttentionPool2d(nn.Module):
         )
         return x.squeeze(0)
 
+
 class ModifiedResNet(nn.Module):
     """
     A ResNet class that is similar to torchvision's but contains the following changes:
@@ -182,6 +188,7 @@ class ModifiedResNet(nn.Module):
         x = self.attnpool(x)
 
         return x
+
 
 class LayerNorm(nn.LayerNorm):
     """Subclass torch's LayerNorm to handle fp16."""
@@ -267,6 +274,7 @@ class VisionTransformer(nn.Module):
             x = x @ self.proj
 
         return x
+
 
 class CLIP(nn.Module):
     def __init__(self,
@@ -399,12 +407,151 @@ class CLIP(nn.Module):
         # shape = [global_batch_size, global_batch_size]
         return logits_per_image, logits_per_text
 
+@lru_cache()
+def default_bpe():
+    return os.path.join(model_folder, "bpe_simple_vocab_16e6.txt.gz")
+
+@lru_cache()
+def bytes_to_unicode():
+    """
+    Returns list of utf-8 byte and a corresponding list of unicode strings.
+    The reversible bpe codes work on unicode strings.
+    This means you need a large # of unicode characters in your vocab if you want to avoid UNKs.
+    When you're at something like a 10B token dataset you end up needing around 5K for decent coverage.
+    This is a signficant percentage of your normal, say, 32K bpe vocab.
+    To avoid that, we want lookup tables between utf-8 bytes and unicode strings.
+    And avoids mapping to whitespace/control characters the bpe code barfs on.
+    """
+    bs = list(range(ord("!"), ord("~")+1))+list(range(ord("¡"), ord("¬")+1))+list(range(ord("®"), ord("ÿ")+1))
+    cs = bs[:]
+    n = 0
+    for b in range(2**8):
+        if b not in bs:
+            bs.append(b)
+            cs.append(2**8+n)
+            n += 1
+    cs = [chr(n) for n in cs]
+    return dict(zip(bs, cs))
+
+def get_pairs(word):
+    """Return set of symbol pairs in a word.
+    Word is represented as tuple of symbols (symbols being variable-length strings).
+    """
+    pairs = set()
+    prev_char = word[0]
+    for char in word[1:]:
+        pairs.add((prev_char, char))
+        prev_char = char
+    return pairs
+
+def basic_clean(text):
+    text = ftfy.fix_text(text)
+    text = html.unescape(html.unescape(text))
+    return text.strip()
+
+
+def whitespace_clean(text):
+    text = re.sub(r'\s+', ' ', text)
+    text = text.strip()
+    return text
+
+
+class SimpleTokenizer(object):
+    def __init__(self, bpe_path: str = default_bpe()):
+        self.byte_encoder = bytes_to_unicode()
+        self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
+        merges = gzip.open(bpe_path).read().decode("utf-8").split('\n')
+        merges = merges[1:49152-256-2+1]
+        merges = [tuple(merge.split()) for merge in merges]
+        vocab = list(bytes_to_unicode().values())
+        vocab = vocab + [v+'</w>' for v in vocab]
+        for merge in merges:
+            vocab.append(''.join(merge))
+        vocab.extend(['<|startoftext|>', '<|endoftext|>'])
+        self.encoder = dict(zip(vocab, range(len(vocab))))
+        self.decoder = {v: k for k, v in self.encoder.items()}
+        self.bpe_ranks = dict(zip(merges, range(len(merges))))
+        self.cache = {'<|startoftext|>': '<|startoftext|>', '<|endoftext|>': '<|endoftext|>'}
+        self.pat = re.compile(r"""<\|startoftext\|>|<\|endoftext\|>|'s|'t|'re|'ve|'m|'ll|'d|[\p{L}]+|[\p{N}]|[^\s\p{L}\p{N}]+""", re.IGNORECASE)
+
+    def bpe(self, token):
+        if token in self.cache:
+            return self.cache[token]
+        word = tuple(token[:-1]) + ( token[-1] + '</w>',)
+        pairs = get_pairs(word)
+
+        if not pairs:
+            return token+'</w>'
+
+        while True:
+            bigram = min(pairs, key = lambda pair: self.bpe_ranks.get(pair, float('inf')))
+            if bigram not in self.bpe_ranks:
+                break
+            first, second = bigram
+            new_word = []
+            i = 0
+            while i < len(word):
+                try:
+                    j = word.index(first, i)
+                    new_word.extend(word[i:j])
+                    i = j
+                except:
+                    new_word.extend(word[i:])
+                    break
+
+                if word[i] == first and i < len(word)-1 and word[i+1] == second:
+                    new_word.append(first+second)
+                    i += 2
+                else:
+                    new_word.append(word[i])
+                    i += 1
+            new_word = tuple(new_word)
+            word = new_word
+            if len(word) == 1:
+                break
+            else:
+                pairs = get_pairs(word)
+        word = ' '.join(word)
+        self.cache[token] = word
+        return word
+
+    def encode(self, text):
+        bpe_tokens = []
+        text = whitespace_clean(basic_clean(text)).lower()
+        for token in re.findall(self.pat, text):
+            token = ''.join(self.byte_encoder[b] for b in token.encode('utf-8'))
+            bpe_tokens.extend(self.encoder[bpe_token] for bpe_token in self.bpe(token).split(' '))
+        return bpe_tokens
+
+    def decode(self, tokens):
+        text = ''.join([self.decoder[token] for token in tokens])
+        text = bytearray([self.byte_decoder[c] for c in text]).decode('utf-8', errors="replace").replace('</w>', ' ')
+        return text
+
+
+
+class hash(Function):
+    @staticmethod
+    def forward(ctx, input):
+        # ctx.save_for_backward(input)
+        return torch.sign(input)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # input,  = ctx.saved_tensors
+        # grad_output = grad_output.data
+
+        return grad_output
+
+def hash_layer(input):
+    return hash.apply(input)
+
+def encode_discrete(x):
+    prob = torch.sigmoid(x)
+    z = hash_layer(prob - 0.5)
+    return z
 
 def load_model(download_root: str = None, model_name: str = "ViT-B/32", jit: bool=False):
-    # model, _ = clip.load("ViT-B/32", device=device, jit=True)
-
-    # import time
-    # start_time = time.time()
     model_ = model[model_name]
     if not model_:
         model_path = os.path.join(model_folder, "ViT-B_32.pth")
@@ -431,21 +578,9 @@ def load_model(download_root: str = None, model_name: str = "ViT-B/32", jit: boo
             if key in state_dict:
                 del state_dict[key]
 
-        # model = CLIP()
         model_.load_state_dict(state_dict, device)
         model_ = model_.to(device)
         model_.eval()
-    # if not model:
-    #     model_path = _download(_MODELS[model_name], download_root or os.path.expanduser("~/.cache/clip"))
-    #     with open(model_path, 'rb') as opened_file:
-    #     #     # loading JIT archive
-    #         model = torch.jit.load(opened_file, map_location=device if jit else "cpu").eval()
-    #     model_visual[model_name] = model.visual
-
-    # model_path = _download(_MODELS[model_name], download_root or os.path.expanduser("~/.cache/clip"))
-    # with open(model_path, 'rb') as opened_file:
-    # #     # loading JIT archive
-    #     model = torch.jit.load(opened_file, map_location=device if jit else "cpu").eval()
 
     # patch the device names
         device_holder = torch.jit.trace(lambda: torch.ones([]).to(torch.device(device)), example_inputs=[])
@@ -471,90 +606,87 @@ def load_model(download_root: str = None, model_name: str = "ViT-B/32", jit: boo
         patch_device(model_.encode_text)
     model[model_name] = model_
 
-    # print('model download time: ', time.time()-start_time)
-    # patch dtype to float32 on CPU
-    # if str(device) == "cpu":
-    #     float_holder = torch.jit.trace(lambda: torch.ones([]).float(), example_inputs=[])
-    #     float_input = list(float_holder.graph.findNode("aten::to").inputs())[1]
-    #     float_node = float_input.node()
-
-    #     def patch_float(module):
-    #         try:
-    #             graphs = [module.graph] if hasattr(module, "graph") else []
-    #         except RuntimeError:
-    #             graphs = []
-
-    #         if hasattr(module, "forward1"):
-    #             graphs.append(module.forward1.graph)
-
-    #         for graph in graphs:
-    #             for node in graph.findAllNodes("aten::to"):
-    #                 inputs = list(node.inputs())
-    #                 for i in [1, 2]:  # dtype can be the second or third argument to aten::to()
-    #                     if inputs[i].node()["value"] == 5:
-    #                         inputs[i].node().copyAttributes(float_node)
-
-    #     model.apply(patch_float)
-    #     patch_float(model.encode_image)
-    #     patch_float(model.encode_text)
-
-    #     model.float()
-    # model.eval()
     return model_
 
-class hash(Function):
-    @staticmethod
-    def forward(ctx, input):
-        # ctx.save_for_backward(input)
-        return torch.sign(input)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        # input,  = ctx.saved_tensors
-        # grad_output = grad_output.data
-
-        return grad_output
-
-def hash_layer(input):
-    return hash.apply(input)
-
-def encode_discrete(x):
-    prob = torch.sigmoid(x)
-    z = hash_layer(prob - 0.5)
-
-    return z
-
-def inference(image):
+def inference(item):
     model = load_model()
 
-    trans = transforms.Compose(
-        [
-            # transforms.ToPILImage(),
-            transforms.ToTensor(),
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.228, 0.224, 0.225]),
-        ]
-    )
+    if isinstance(item, str):
+        text = tokenize(f"a photo of a {item}").to(device)
+        features = model.encode_text(text)
+    else:
+        trans = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.228, 0.224, 0.225]),
+            ]
+        )
 
-    img = np.uint8(image.data)
-    img = Image.fromarray(img)
+        img = np.uint8(item.data)
+        img = Image.fromarray(img)
 
-    if np.array(img).ndim == 2:
-        img = img.convert('RGB')
-    img = trans(img)
-    img = np.expand_dims(img, axis=0)
-    img = torch.Tensor(img)
-    img = img.to(device, dtype=torch.float)
+        if np.array(img).ndim == 2:
+            img = img.convert('RGB')
+        img = trans(img)
+        img = np.expand_dims(img, axis=0)
+        img = torch.Tensor(img)
+        img = img.to(device, dtype=torch.float)
 
-    img_features = model.encode_image(img)
-    img_features = encode_discrete(img_features)
-    img_features = F.normalize(img_features, dim=-1)
+        features = model.encode_image(img)
 
-    img_features = img_features.cpu()
-    hash_key = img_features.detach().numpy() >= 0
+    features = encode_discrete(features)
+    features = F.normalize(features, dim=-1)
+
+    features = features.cpu()
+    hash_key = features.detach().numpy() >= 0
     hash_key = hash_key*1
     hash_string = np.packbits(hash_key, axis=-1)
     hash_string = list(map(lambda row: ''.join(['{:02x}'.format(r) for r in row]), hash_string))
 
     return hash_string
+
+def tokenize(texts: Union[str, List[str]], context_length: int = 77, truncate: bool = False) -> Union[torch.IntTensor, torch.LongTensor]:
+    """
+    Returns the tokenized representation of given input string(s)
+
+    Parameters
+    ----------
+    texts : Union[str, List[str]]
+        An input string or a list of input strings to tokenize
+
+    context_length : int
+        The context length to use; all CLIP models use 77 as the context length
+
+    truncate: bool
+        Whether to truncate the text in case its encoding is longer than the context length
+
+    Returns
+    -------
+    A two-dimensional tensor containing the resulting tokens, shape = [number of input strings, context_length].
+    We return LongTensor when torch version is <1.8.0, since older index_select requires indices to be long.
+    """
+    if isinstance(texts, str):
+        texts = [texts]
+    
+    _tokenizer = SimpleTokenizer()
+
+    sot_token = _tokenizer.encoder["<|startoftext|>"]
+    eot_token = _tokenizer.encoder["<|endoftext|>"]
+    all_tokens = [[sot_token] + _tokenizer.encode(text) + [eot_token] for text in texts]
+    if packaging.version.parse(torch.__version__) < packaging.version.parse("1.8.0"):
+        result = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
+    else:
+        result = torch.zeros(len(all_tokens), context_length, dtype=torch.int)
+
+    for i, tokens in enumerate(all_tokens):
+        if len(tokens) > context_length:
+            if truncate:
+                tokens = tokens[:context_length]
+                tokens[-1] = eot_token
+            else:
+                raise RuntimeError(f"Input {texts[i]} is too long for context length {context_length}")
+        result[i, :len(tokens)] = torch.tensor(tokens)
+
+    return result
--- datumaro/components/searcher.py
+++ datumaro/components/searcher.py
@@ -2,14 +2,13 @@
 #
 # SPDX-License-Identifier: MIT
 
-from typing import Optional
+from typing import Optional, Union, List
 
 import numpy as np
 
-import torch
-
 from datumaro.components.dataset import IDataset
 from datumaro.components.extractor import DatasetItem
+from datumaro.components.model_inference import inference
 
 
 def calculate_hamming(B1, B2):
@@ -20,30 +19,7 @@ def calculate_hamming(B1, B2):
     """
     q = B2.shape[1] # max inner product value
     distH = 0.5 * (q - B1@B2.transpose())
-    # distH = q - B1@B2.transpose()
     return distH
-
-def hamming_distance(chaine1, chaine2):
-    return sum(c1 != c2 for c1, c2 in zip(chaine1, chaine2))
-
-# class Solution(object):
-#    def hammingDistance(self, x, y):
-#       """
-#       :type x: int
-#       :type y: int
-#       :rtype: int
-#       """
-#       ans = 0
-#       for i in range(31,-1,-1):
-#          b1= x>>i&1
-#          b2 = y>>i&1
-#          ans+= not(b1==b2)
-#          #if not(b1==b2):
-#             # print(b1,b2,i)
-#       return ans
-# ob1 = Solution()
-# print(ob1.hammingDistance(7, 15))
-
 
 
 class Searcher:
@@ -57,63 +33,48 @@ class Searcher:
 
         Parameters
         ----------
-        dataset:
-            Datumaro dataset to search similar dataitem.
+        dataset: Datumaro dataset to search similar dataitem.
         topK:
-
         """
         self._dataset = dataset
         self._topk = topk
 
-    def search_topk(self, item: DatasetItem, topk: Optional[int]=None):
+    def unpack_hash_key(self, hash_key: List):
+        """
+        """
+        hash_key_list = [hash_key[i:i+2] for i in range(0, len(hash_key), 2)]
+        hash_key = np.array([int(s, 16) for s in hash_key_list], dtype='uint8')
+        hash_key = np.unpackbits(hash_key, axis=-1)
+        return hash_key
+
+    def search_topk(self, query: Union[DatasetItem, str], topk: Optional[int]=None):
         """
         Search topk similar results based on hamming distance for query DatasetItem
         """
         if not topk:
             topk = self._topk
 
-        # query_key = np.array([int(item.hash_key[0], 36)])
-        query_key = item.hash_key[0]
-        query_key_list = [query_key[i:i+2] for i in range(0, len(query_key), 2)]
-        query_key = np.array([int(s, 16) for s in query_key_list], dtype='uint8')
-        query_key = np.unpackbits(query_key, axis=-1)
-        # query_key = np.reshape(query_key, (1, -1))
-        # query_key_string = np.packbits(query_key, axis=-1)
-        # query_key_string = list(map(lambda row: ''.join(['{:02x}'.format(r) for r in row]), [query_key_string]))
+        if isinstance(query, DatasetItem):
+            query_key = query.hash_key[0]
+        elif isinstance(query, str):
+            query_key = inference(query)[0]
+        else:
+            raise ValueError("Query should be DatasetItem or string")
+
+        query_key = self.unpack_hash_key(query_key)
 
         retrieval_keys = []
-        # retrieval_keys_string =[]
-        # id_list = []
         item_list = []
         for datasetitem in self._dataset:
-            # hash_key = int(datasetitem.hash_key[0], 36)
             hash_key = datasetitem.hash_key[0]
-            hash_key_list = [hash_key[i:i+2] for i in range(0, len(hash_key), 2)]
-            hash_key = np.array([int(s, 16) for s in hash_key_list], dtype='uint8')
-            hash_key = np.unpackbits(hash_key, axis=-1)
-            # hash_key = np.reshape(hash_key, (1, -1))
-            # hash_string = np.packbits(hash_key, axis=-1)
-            # hash_string = list(map(lambda row: ''.join(['{:02x}'.format(r) for r in row]), [hash_string]))
+            hash_key = self.unpack_hash_key(hash_key)
             retrieval_keys.append(hash_key)
-            # retrieval_keys_string.append(hash_string)
-            # id_list.append(datasetitem.id)
             item_list.append(datasetitem)
         
         retrieval_keys = np.stack(retrieval_keys, axis=0)
-        # retrieval_keys_string = np.stack(retrieval_keys_string, axis=0)
-        # retrieval_keys = torch.stack(retrieval_keys, dim=1).to(device = "cpu")
-
-        # logits = 100. * query_key @ retrieval_keys
 
         logits = calculate_hamming(query_key, retrieval_keys)
-        # pred = logits.topk(topk, 0, True, True)[1]
         ind = np.argsort(logits)
-        # retrieval_keys = retrieval_keys[ind] # reorder gnd
-
-        # tgnd = retrieval_keys[0:topk]
-        
-        # id_list = np.array(id_list)[ind]
-        # result = id_list[:topk].tolist()
 
         item_list =np.array(item_list)[ind]
         result = item_list[:topk].tolist()
